f4b378e80b5c946cb8ba61d8a74d3a4346dc41b4
Jerry <jerry.pon@utexas.edu>
2014-11-16 21:54:31 -0600

    99Balloons
    A simple Xcode program that uses UI transition.

---
+++ 99 Red Balloons/Balloon.swift
@@ -0,0 +1,15 @@
+//
+//  Balloon.swift
+//  99 Red Balloons
+//
+//  Created by Jerry on 11/13/14.
+//  Copyright (c) 2014 Jerry. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Balloon {
+    var count = 0
+    var image = UIImage(named: "")
+}
